551c01aa8e14f11802c07e9dbacc371f52cdebf8
Michał Byszof <nietrwalyenol@users.noreply.github.com>
2017-03-07 16:31:44 +0700

    Add files via upload

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+def create
+  @article = Article.new(article_params)
+ 
+  @article.save
+  redirect_to @article
+end
+ 
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+  def new
+  end
+
